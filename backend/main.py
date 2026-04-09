37ec98c07ed3b61aba6ca461ca874715dc288f73
drewgarcia725 <drewgarcia725@gmail.com>
2026-04-09 09:23:24 +0800

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,1 +1,17 @@
+from fastapi import FastAPI, Depends, Header, HTTPException
+from sqlmodel import Session, select
+from database import get_session, init_db
+from models import *
+from typing import Optional
 
+app = FastAPI()
+
+@app.on_event("startup")
+def on_startup():
+    init_db()
+
+def get_current_user(x_user_id: str = Header(...), session: Session = Depends(get_session)):
+    user = session.get(User, x_user_id)
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    return user
