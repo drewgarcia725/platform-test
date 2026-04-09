c45b4e7c57981b1ea97a1e3bd8bf23793c047086
drewgarcia725 <drewgarcia725@gmail.com>
2026-04-09 10:49:40 +0800

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, Depends, Header, HTTPException
 from sqlmodel import Session, select
 from database import get_session, init_db
+from uuid import UUID
 from models import *
 from typing import Optional
 
@@ -21,13 +22,14 @@ def get_current_user(x_user_id: str = Header(...), session: Session = Depends(ge
 def get_user(session, user_id):
     return session.get(User, user_id)
 
-def get_user_client_ids(session, user_id):
-    assignments = session.exec(
-        select(UserClientAssignment).where(
+def get_user_client_ids(session, user_id: UUID):
+    results = session.exec(
+        select(UserClientAssignment.client_id).where(
             UserClientAssignment.user_id == user_id
         )
     ).all()
-    return [a.client_id for a in assignments]
+
+    return results
 
 def check_access(user, client_id, allowed_clients):
     if user.role == "director":
