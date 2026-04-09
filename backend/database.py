9644065c9584a458dff4fd677d7616da13de52c8
drewgarcia725 <drewgarcia725@gmail.com>
2026-04-09 09:21:47 +0800

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -1,1 +1,10 @@
+from sqlmodel import SQLModel, create_engine, Session
 
+engine = create_engine("sqlite:///database.db", echo=False)
+
+def get_session():
+    with Session(engine) as session:
+        yield session
+
+def init_db():
+    SQLModel.metadata.create_all(engine)
